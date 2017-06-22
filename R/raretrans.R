7b7daf3be8668a5a364dae72411a62562552ac87
Drew Tyre <atyre2@unl.edu>
2017-06-22 12:04:20 -0500

    add more to description in package doc

--- R/raretrans.R
+++ R/raretrans.R
@@ -1,7 +1,10 @@
 #' raretrans: A package for creating and updating Bayesian priors for matrix population models
 #'
 #' This package provides functions that combine data on transitions with prior beliefs about
-#' transition probabilities and fertilities.
+#' transition probabilities and fertilities. Includes functions to create matrix population models from a combination
+#' of data on stage/age transitions and Bayesian prior information. This
+#' compensates for structural problems caused by missing observatons of rare
+#' transitions.
 #'
 #' @docType package
 #' @name raretrans
