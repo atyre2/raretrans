378af0a17cbcb3983bfcef3d836ed9809d05cfc2
Drew Tyre <atyre2@unl.edu>
2017-07-16 17:37:03 -0500

    Transfer most of prior testing to vignette and tweak fill_transition() arguments

--- R/fill_funcs.R
+++ R/fill_funcs.R
@@ -7,8 +7,8 @@
 #' not yet use the prior argument P.
 #'
 #' @param TF A list of two matrices, T and F, as ouput by \code{\link[popbio]{projection.matrix}}.
-#' @param P A matrix of the priors for each column.
 #' @param N A vector of observed transitions.
+#' @param P A matrix of the priors for each column.
 #' @param returnType A character vector describing the desired return value.
 #'
 #' @return The return value depends on parameter returnType.
@@ -20,7 +20,7 @@
 #'
 #' @export
 #'
-fill_transitions <- function(TF, P, N, returnType = "A"){
+fill_transitions <- function(TF, N, P, returnType = "A"){
   Tmat <- TF$T
   Fmat <- TF$F
   order <- dim(Tmat)[1]
@@ -86,3 +86,42 @@ fill_fecundity <- function(TF, P, N, returnType = "A"){
     stop("Bad returntype in fill_fecundity()")
   }
 }
+
+## helper functions for generating different priors
+#' Extract the number of individuals in each stage from a dataframe
+#' of transitions
+#'
+#' @param transitions a dataframe of observations of individuals in different stages
+#' @param stage the name of the variable with the stage information
+#' @param sort a vector of stage names in the desired order. Default is the order of levels in stage.
+#'
+#' @return a vector of the counts of observations in each level of stage.
+#' @export
+#'
+
+get_state_vector <- function (transitions, stage = NULL,
+                              sort = NULL)
+{
+  if (missing(stage)) {
+    stage <- "stage"
+  }
+  nl <- as.list(1:ncol(transitions))
+  names(nl) <- names(transitions)
+  stage <- eval(substitute(stage), nl, parent.frame())
+  if (is.null(transitions[, stage])) {
+    stop("No stage column matching ", stage)
+  }
+  if (missing(sort)) {
+    sort <- levels(transitions[, stage])
+  }
+  tf <- table(transitions[, stage])[sort]
+  return(as.vector(tf))
+}
+
+## testing
+# df_5 %>%
+#   mutate(stage = factor(stage, levels=c("p","j","a","m")),
+#          fate = factor(next_stage, levels=c("p","j","a","m"))) %>%
+#   filter(POPNUM==209, year==1) %>%
+#   as.data.frame() %>%
+#   get_state_vector(sort=c("p","j","a"))
